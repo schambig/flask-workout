6f4282d8cad9301176a27c15a59ea7e274b80588
Salomon Chambi <salomonchambi@gmail.com>
2023-08-11 19:26:40 -0500

    feat: run a flask app and template

---
+++ demo/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'Hello World!'
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
